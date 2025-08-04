fce9b8b1c6bdf21e78e173e5e940786691a1ba74
WoodShack <woodhousie@gmail.com>
2025-08-04 13:07:05 -0400

    Task 3: Send Data Using POST

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let fetchBtn = document.getElementById("fetch-btn");
 let xhrBtn = document.getElementById("xhr-btn");
 let dataDiv = document.getElementById("data");
 let errorDiv = document.getElementById("error");
+let form = document.getElementById("form");
 
 //Event listeners
 fetchBtn.addEventListener("click", function() {
@@ -36,6 +37,17 @@ xhrBtn.addEventListener("click", function() {
     xhr.send();
 });
 
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const type = formData.get("type");
+    const title = formData.get("title");
+    const body = formData.get("body");
+    
+    sendPost(title,body);
+});
+
 //Functions
 function showError(errorMessage){
     errorDiv.innerHTML = errorMessage;
@@ -48,4 +60,28 @@ function populateDataDiv(object){
             dataDiv.innerHTML += `<p><strong>${key}:</strong> ${object[key]}</p>`;
         }
     }
+}
+
+function sendPost(title,body){
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: title,
+            body: body
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            showError("Network response was not ok");
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        populateDataDiv(data);
+    })
+    .catch(error => showError('Error fetching data'));
 }
